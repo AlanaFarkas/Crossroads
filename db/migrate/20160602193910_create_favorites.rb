aea1a4562a1230c2cf36e29a186ec0e9ff5fc4fb
Leo Kukhar <leokukhar@gmail.com>
2016-06-02 16:06:53 -0400

    Add Migrations

--- db/migrate/20160602193910_create_favorites.rb
+++ db/migrate/20160602193910_create_favorites.rb
@@ -1,6 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration
   def change
     create_table :favorites do |t|
+      t.references :user, foreign_key: true, index: true
+      t.references :establishment, foreign_key: true, index: true
 
       t.timestamps null: false
     end
