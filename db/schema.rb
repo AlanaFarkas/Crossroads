5094d43964ec0084b879fcca530e551763ffe36c
Leo Kukhar <leokukhar@gmail.com>
2016-06-02 21:17:09 -0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 20160602193926) do
   add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "uid"
     t.string   "first_name",                          null: false
     t.string   "last_name",                           null: false
     t.string   "email",                  default: "", null: false
