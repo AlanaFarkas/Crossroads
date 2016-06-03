c37be93c310e347c16cf9472949332f827bf920d
RobertReith <robertmreith@gmail.com>
2016-06-02 20:10:05 -0400

    Continue to work on devise and omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,26 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  devise :omniauthable, :omniauth_providers => [:google_oauth2]
+
   validates_presence_of :first_name, :last_name, :email
   validates_uniqueness_of :email
   validates_format_of :email,  with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates_length_of :password, within: 6..25, too_long: 'cannot be over 25 characters', too_short: 'must be at least 6 characters'
 
-  has_secure_password
   has_many :addresses
   has_many :favorites
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+        user = User.create(name: data["name"],
+           email: data["email"],
+           password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+  end
 end
