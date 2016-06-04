f9517efdf4dc6d6d6dfaddbadc75ffa652ddd5d0
Leo Kukhar <leokukhar@gmail.com>
2016-06-03 22:04:19 -0400

    Add Search Routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :favorites, only: [:index]
   resources :establishments, only: [:show]
+  resources :search , only: [:new, :show]
 
   devise_scope :user do
     # get '/login' => 'sessions#new'
