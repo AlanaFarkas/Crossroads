d108022d4457676a3dc53af750d08c1a2dff7121
Ayaz Uddin <ayaz2589@gmail.com>
2016-06-04 21:30:28 -0400

    add address controllers and view

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,34 @@
+class AddressesController < ApplicationController
+	def index
+		@addresses = Address.all
+	end
+
+	def new
+		@address = Address.new
+	end
+
+	def create
+		@address = Address.create(addresses_params)
+		current_user.addresses << @address
+		if @address.save
+			redirect_to user_path(current_user.id)
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def addresses_params
+		params.require(:address).permit(:address_1, :address_type, :user_id, :city, :state, :zip)
+	end
+end
--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,11 @@ class Address < ActiveRecord::Base
   validates_presence_of :address_1, :city, :state, :zip
 
   belongs_to :user
+
+  def full_address
+  	"#{self.address_1} #{self.city} #{self.state} #{self.zip}"
+  end
+
+  ADDRESS_TYPES = ["Home", "Work"]
+
 end
