dc720bedb904e72c86e5a596088f6a76de0396a9
Alana Farkas <alana.farkas@gmail.com>
2016-06-05 17:59:24 -0400

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+20.times do
+  User.create!(first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.phone_number,
+    password: "Password123")
+end
+
+35.times do
+	Address.create!(address_1: Faker::Address.street_address,
+    address_2: Faker::Address.secondary_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zip: Faker::Address.zip,
+    address_type: "Home",
+    user: User.all.sample)
+end
+
+50.times do
+   Establishment.create!(name: Faker::Name.first_name,
+    address_1: Faker::Address.street_address,
+    address_2: Faker::Address.secondary_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zip: Faker::Address.zip,
+    phone_number: Faker::PhoneNumber.phone_number,
+    website: Faker::Internet.url)
+end
+
+100.times do
+	fav = Favorite.create(user: User.all.sample,
+		establishment: Establishment.all.sample)
+end
