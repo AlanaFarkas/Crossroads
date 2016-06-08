6d86aefc3a4e9b904fc90ab07e70564baa1e40b3
RobertReith <robertmreith@gmail.com>
2016-06-08 11:05:04 -0400

    Add custom midpoint marker sized correctly

--- app/assets/javascripts/directions.js
+++ app/assets/javascripts/directions.js
@@ -93,13 +93,14 @@
           var lng = path[middleOfArray].lng();
           midpoint = {lat: lat, lng: lng};
 
-          // var image =  {url: "/imgs/compass-marker.jpg",
-          //             scaledSize: new google.maps.Size(42, 68)
-          //           }
+          var image = {url: "/imgs/midpoint-marker.png",
+                       scaledSize: new google.maps.Size(38, 60)
+                      }
+
           var midpointMarker = new google.maps.Marker({
             position: midpoint,
             map: map,
-            icon: "/imgs/marker.jpg"
+            icon: image
           });
 
           var radialBounds = new google.maps.Circle({
